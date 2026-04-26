c67738dd70426199835449677b0c3578e58d528a
Saumya-bhardwaj04 <samisharma000@gmail.com>
2026-04-26 23:40:41 +0530

    fix: bind to 0.0.0.0

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 import express from "express";
 const app = express();
-
+const PORT = process.env.PORT || 9000;
 app.use(express.json());
 
 app.get("/",(req,res)=>{
     res.send("hello");
 })
-app.listen(9000,()=>{
-    console.log("server running on 9000");
+app.listen(PORT,"0.0.0.0",()=>{
+    console.log(`server running on port ${PORT}`);
 })
