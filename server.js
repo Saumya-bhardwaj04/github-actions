26a052ee4d5664e502228ebf3a79df6d4c667027
Saumya-bhardwaj04 <samisharma000@gmail.com>
2026-04-26 23:46:50 +0530

    trigger workflow

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 9000;
 app.use(express.json());
 
 app.get("/",(req,res)=>{
-    res.send("hello");
+    res.send("CI/CD test working 🔥");
 })
 app.listen(PORT,"0.0.0.0",()=>{
     console.log(`server running on port ${PORT}`);
